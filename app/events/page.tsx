61054558546396e68c6c99695302a805157e6441
TimeSlayer <nikitajudo313@gmail.com>
2025-05-28 16:15:05 +0300

    fix: wrap Filters and EventCards in Suspense

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,14 +1,17 @@
 import styles from '@/app/ui/events/events.module.css';
 import Filters from '@/app/ui/events/filters';
 import EventCards from '../ui/events/event-cards';
+import { Suspense } from 'react';
 
 export default function Page() {
   return (
     <main className={styles['main']}>
       <section className={`container ${styles['section']}`}>
         <h2 className={styles['section-title']}>Події нашої академії</h2>
-        <Filters />
-        <EventCards />
+        <Suspense>
+          <Filters />
+          <EventCards />
+        </Suspense>
       </section>
     </main>
   );
