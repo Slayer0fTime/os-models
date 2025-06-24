616f1b1ddc1f8e0b0e91900678c46e1fb90b5cf6
TimeSlayer <nikitajudo313@gmail.com>
2025-06-24 14:32:20 +0300

    fix: adjust map size for responsive layout in location section

--- app/ui/main/location-section.tsx
+++ app/ui/main/location-section.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import styles from './main.module.css';
 import Image from 'next/image';
+import locationImage from '@/public/main/location.png';
 
 export default function LocationSection() {
   return (
@@ -14,7 +15,11 @@ export default function LocationSection() {
             className={styles['location-map']}
             target="_blank"
             href="https://www.google.com/maps/place/Vasylkivska+St,+34,+Kyiv,+02000/@50.3935707,30.4825893,17z/data=!3m1!4b1!4m6!3m5!1s0x40d4c8daee2e02bd:0x309b0427b63c50c6!8m2!3d50.3935673!4d30.4851696!16s%2Fg%2F11c43wgmfb?entry=ttu&g_ep=EgoyMDI1MDUyNy4wIKXMDSoASAFQAw%3D%3D">
-            <Image alt="Location" src="/main/location.png" fill style={{ objectFit: 'cover' }} />
+            <Image
+              alt="Location"
+              src={locationImage}
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
           </a>
           <div className={styles['location-info']}>
             <p>Васильківська вул. 34</p>
